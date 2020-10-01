b509eb2045be8742e472a5f3d8c5cb0776d512e2
Koraltan Kaynak <koraltan.kaynak@deutschebahn.com>
2020-10-02 00:29:51 +0200

    update run

--- src/indy-material/nodejs/config.js
+++ src/indy-material/nodejs/config.js
@@ -12,19 +12,19 @@ const config = {
     port: process.env.PORT || 3000,
 
     // Optional: Give your wallet a unique name
-    walletName: `${process.env.USERNAME || 'alice'}_wallet`,
+    walletName: `${process.env.USERNAME || 'asset'}_wallet`,
 
     // Optional: Give your pool config a unique name
     poolName: process.env.POOL_NAME || 'pool1',
 
     // This information is used to issue your "Government ID"
     userInformation: {
-        name: process.env.NAME || 'Alice Garcia',
-        email: process.env.EMAIL || 'alice@faber.edu',
-        tax_id: process.env.TAX_ID || '123-45-6789',
-        icon_src: process.env.ICON_SRC || 'https://cdn3.iconfinder.com/data/icons/black-easy/512/538738-school_512x512.png',
-        username: process.env.USERNAME || 'alice',
-        password: process.env.PASSWORD || '123'
+        name: process.env.NAME || 'Asset',
+        email: process.env.EMAIL || 'drom@deutschebahn.com',
+        tax_id: process.env.TAX_ID || '999-55-3005',
+        icon_src: process.env.ICON_SRC || 'https://www.psdgraphics.com/wp-content/uploads/2019/12/christmas-squares-pattern-seamless.png',
+        username: process.env.USERNAME || 'asset',
+        password: process.env.PASSWORD || '2020'
     },
 
     sessionSecret: "YUYFDISYFSIUOFYERTEWRTEWTWETRNNNMNJHKHFASDdyfiudayDAYIUSDFYASIOFOOASIUDFYEREAHLSKJFE57894502354354HJKAFDDFS"
--- up.sh
+++ up.sh
@@ -19,13 +19,18 @@ trap_ctrlc()
 trap "trap_ctrlc" 2
 
 clear
-echo "Running Up Container with Wallets:"
-echo "-------*------*----------------------------------------"
-echo " Alice : 3005 | https://alice.wallet.drom.beta.de.com"
-echo " Bob   : 3001 | https://bob.wallet.drom.beta.de.com"
-echo " Faber : 3002 | https://faber.wallet.drom.beta.de.com"
-echo " Acme  : 3003 | https://acme.wallet.drom.beta.de.com"
-echo " Bank  : 3004 | https://bank.wallet.drom.beta.de.com"
-echo "-------*------*----------------------------------------"
+echo "---------------------------------------------------------------------------"
+echo " Running Up Container with Wallets"
+echo "----------+------+------+--------------------------------------------------"
+echo " Asset    : 3005 | https://wallet.asset.drom.beta.de.com"
+echo " Roland   : 3001 | https://wallet.roland.drom.beta.de.com"
+echo " Marta    : 3002 | https://wallet.marta.drom.beta.de.com"
+echo " Luisa    : 3003 | https://wallet.luisa.drom.beta.de.com"
+echo " Andrei   : 3004 | https://wallet.andrei.drom.beta.de.com"
+echo " Cemil    : 3006 | https://wallet.cemil.drom.beta.de.com"
+echo " Koraltan : 3007 | https://wallet.koraltan.drom.beta.de.com"
+echo "----------+------+------+--------------------------------------------------"
+echo " Ledger Discover : 9000 | https://wallet.ledger-explorer.drom.beta.de.com"
+echo "----------+------+------+--------------------------------------------------"
 cd "src/indy-material/nodejs"
 docker-compose up -d
