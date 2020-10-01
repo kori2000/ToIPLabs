060fb9bf91b8fe7db344c674237dda1a775d2e72
Koraltan Kaynak <koraltan.kaynak@deutschebahn.com>
2020-10-01 20:01:35 +0200

    added run script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+echo "Building DID Demo..."
+sh ./src/indy-material/nodejs/manage build
+echo "Done."
+echo "Running Container:"
+echo "------*--------------------"
+echo "Alice : PORT = 3000 "
+echo "Bob   : PORT = 3001 "
+echo "Faber : PORT = 3002 "
+echo "Acme  : PORT = 3003 "
+echo "Bank  : PORT = 3004 "
+echo "------*--------------------"
+sh ./src/indy-material/nodejs/manage up
